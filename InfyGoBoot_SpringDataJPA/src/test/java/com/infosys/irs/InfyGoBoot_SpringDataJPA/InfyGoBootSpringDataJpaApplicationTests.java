226f3e29be259ac8377a520522c672a70ba862c8
Manish Kumar <50228703+Manishk1095@users.noreply.github.com>
2020-09-01 01:35:29 +0530

    Commit All files

---
+++ InfyGoBoot_SpringDataJPA/src/test/java/com/infosys/irs/InfyGoBoot_SpringDataJPA/InfyGoBootSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.irs.InfyGoBoot_SpringDataJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyGoBootSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
